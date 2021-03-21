53540b724b3134332bf4c3da4c149625fb655a77
aisheryo adnan <joseph.nasif@gmail.com>
2021-03-21 16:15:17 +0600

    lab 1

---
+++ AccountTransections/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTransections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account s1 = new Account("Adnan", "11111", 2123131);
+            Account s2 = new Account("nasif", "22222", 10000);
+            Account r1 = new Account("Aisheryo", "33333", 5000);
+            Account r2 = new Account("surjo", "4444", 20000);
+
+            Transections t1 = new Transections(s1, r1, 219, "send money");
+            Transections t2 = new Transections(s1, r2, 200, "send money");
+
+            s1.addTransaction(t1, t2);
+            s1.showAllTransection();
+            s1.ShowInfo();
+            s1.Transfer(r2, 200);
+        }
+    }
+}
--- AccountTransections/Properties/AssemblyInfo.cs
+++ AccountTransections/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("final lab 1")]
+[assembly: AssemblyTitle("AccountTransections")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("final lab 1")]
+[assembly: AssemblyProduct("AccountTransections")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a1165820-7855-4e71-a390-5b9dfb3d38da")]
+[assembly: Guid("2d9a4f61-647f-447d-b97b-aa1125f5c760")]
 
 // Version information for an assembly consists of the following four values:
 //
