5e6b641da5d019a0ae1a5a9206224fa9df81aeab
aisheryo adnan <joseph.nasif@gmail.com>
2021-03-28 15:16:11 +0600

    lab 7

---
+++ LAB 7 TestInterface/TestInterface/BasicBankingInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInterface
+{
+    interface BasicBankingInterface
+        {
+            void deposit(int amount);
+            void widhdraw(int amount);
+        }
+    
+}
+
---
+++ LAB 7 TestInterface/TestInterface/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+               
+                {
+                    Account a1 = new Savings("Adnun", "56567", 600000);
+                    Account a2 = new Current("Shurjo", "17761", 1230000);
+                    Account a3 = new Overdraft("Nasif", "23876", 5500, 5120);
+
+                    a1.ShowInfo();
+                    a1.deposit(2000);
+                    a3.widhdraw(21000);
+
+
+                }
+            
+        }
+
+    }
+}
+
